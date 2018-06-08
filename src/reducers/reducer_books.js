bbf040468ea5e891cdef0f853a077e27032e1b0e
AkkaFranKebne <ewawrobel@gmail.com>
2018-06-08 13:30:47 +0200

    rendering list of books using redux

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,11 @@
+export default function() {
+  return [
+    { title: 'Book 1' },
+    { title: 'Book 2' },
+    { title: 'Book 3' },
+    { title: 'Book 4' },
+    { title: 'Book 5' },
+    { title: 'Book 6' },
+    { title: 'Book 7' }
+  ]
+}
